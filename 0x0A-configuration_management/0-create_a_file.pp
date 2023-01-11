a2b7b999b05fcfb87afe0080787756fd639fac47
technomusa <52075608+technomusa@users.noreply.github.com>
2023-01-11 17:41:36 +0000

    error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #Using Puppet, create a file in /tmp
 
 file {
-  '/tmp/holberton':
-  path    =>  '/tmp/holberton',
+  '/tmp/school':
+  path    =>  '/tmp/school',
   mode    =>  '0744',
   owner   =>  'www-data',
   group   =>  'www-data',
