c7271732392372bc29c4fa674088110f5ea3e668
technomusa <52075608+technomusa@users.noreply.github.com>
2023-02-10 12:27:23 +0000

    initial

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""export data in the JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    with open('todo_all_employees.json', mode='w') as employement:
+        employees = requests.get(
+            'https://jsonplaceholder.typicode.com/users/').json()
+        for employee in employees:
+            ids = employee.get('id')
+            todos = requests.get(
+                'https://jsonplaceholder.typicode.com/users/{}/todos'
+                .format(ids)).json()
+            tasks = []
+            json_file = {}
+            for task in todos:
+                tasks.append({"task": task.get("title"),
+                              "username": employee.get("username"),
+                              "completed": task.get("completed")})
+            json_file.update({ids: tasks})
+        json.dump(json_file, employement)
