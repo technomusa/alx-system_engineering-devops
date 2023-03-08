0513c499bc46f83a1fdd0446ef7b34446f90f07e
technomusa <52075608+technomusa@users.noreply.github.com>
2023-03-08 13:08:15 +0000

    initiate

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Write a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles
+for a given subreddit. If no results are found for the given subreddit, the function should return None.
+
+Hint: The Reddit API uses pagination for separating pages of responses.
+
+Requirements:
+
+Prototype: def recurse(subreddit, hot_list=[])
+Note: You may change the prototype, but it must be able to be called with just a subreddit supplied.
+AKA you can add a counter, but it must work without supplying a starting value in the main.
+If not a valid subreddit, return None.
+NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
+Your code will NOT pass if you are using a loop and not recursively calling the function! This /can/ be done with a loop but
+the point is to use a recursive function. :)
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
